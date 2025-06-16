925c56446c8f1d4711f3c962605a77855c99155a
marselspk <katutunsambar@gmail.com>
2025-06-16 14:30:38 +0700

    pertemuan 9
    Signed-off-by: marselspk <katutunsambar@gmail.com>

--- resources/views/v_order/select_payment.blade.php
+++ resources/views/v_order/select_payment.blade.php
@@ -1,30 +1,23 @@
 @extends('v_layouts.app')
 @section('content')
-<!-- template -->
 <div class="col-md-12">
     <div class="order-summary clearfix">
         <div class="section-title">
             <p>KERANJANG</p>
             <h3 class="title">Keranjang Belanja</h3>
         </div>
-        <!-- msgSuccess -->
         @if(session()->has('success'))
         <div class="alert alert-success alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" arialabel="Close">
-                <span aria-hidden="true">&times;</span></button>
+            <button type="button" class="close" data-dismiss="alert" arialabel="Close"><span aria-hidden="true">&times;</span></button>
             <strong>{{ session('success') }}</strong>
         </div>
         @endif
-        <!-- end msgSuccess -->
-        <!-- msgError -->
         @if(session()->has('error'))
         <div class="alert alert-danger alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" arialabel="Close">
-                <span aria-hidden="true">&times;</span></button>
+            <button type="button" class="close" data-dismiss="alert" arialabel="Close"><span aria-hidden="true">&times;</span></button>
             <strong>{{ session('error') }}</strong>
         </div>
         @endif
-        <!-- end msgError -->
         @if($order && $order->orderItems->count() > 0)
         <table class="shopping-cart-table table">
             <thead>
@@ -34,7 +27,6 @@
                     <th class="text-center">Harga</th>
                     <th class="text-center">Quantity</th>
                     <th class="text-center">Total</th>
-                    <th class="text-right"></th>
                 </tr>
             </thead>
             <tbody>
@@ -48,52 +40,56 @@
                 $totalBerat += $item->produk->berat * $item->quantity;
                 @endphp
                 <tr>
-                    <td class="thumb">
-                        <img src="{{ asset('storage/img-produk/thumb_sm_' . $item->produk->foto) }}" alt="">
-                    </td>
+                    <td class="thumb"><img src="{{ asset('storage/img-produk/thumb_sm_' .$item->produk->foto) }}" alt=""></td>
                     <td class="details">
                         <a>{{ $item->produk->nama_produk }}</a>
                         <ul>
                             <li><span>Berat: {{ $item->produk->berat }} Gram</span></li>
                         </ul>
                         <ul>
-                            <li><span>Stok: {{ $item->produk->stok }} Pcs</span></li>
+                            <li><span>Stok: {{ $item->produk->stok }} Gram</span></li>
                         </ul>
                     </td>
-                    <td class="price text-center">
-                        <strong>Rp. {{ number_format($item->harga, 0, ',', '.') }}</strong>
-                    </td>
+                    <td class="price text-center"><strong>Rp. {{ number_format($item->harga, 0, ',', '.') }}</strong></td>
                     <td class="qty text-center">
-                        <form action="{{ route('order.updateCart', $item->id) }}" method="post">
-                            @csrf
-                            <input type="number" name="quantity"
-                                value="{{ $item->quantity }}" min="1" style="width: 60px;">
-                            <button type="submit" class="btn btn-sm btn-warning">Update</button>
-                        </form>
+                        <a> {{ $item->quantity }} </a>
                     </td>
                     <td class="total text-center"><strong class="primary-color">Rp. {{ number_format($item->harga * $item->quantity, 0, ',', '.') }}</strong></td>
-                    <td class="text-right">
-                        <form action="{{ route('order.remove', $item->produk->id) }}" method="post">
-                            @csrf
-                            <button class="main-btn icon-btn"><i class="fa fa-close"></i></button>
-                        </form>
-                    </td>
                 </tr>
                 @endforeach
             </tbody>
+            <tfoot>
+                <tr>
+                    <th class="empty" colspan="3"></th>
+                    <th>SUBTOTAL</th>
+                    <th colspan="2" class="sub-total">Rp. {{ number_format($totalHarga, 0, ',', '.') }}</th>
+                </tr>
+                <tr>
+                    <th class="empty" colspan="3"></th>
+                    <th>Ongkos Kirim</th>
+                    <td colspan="2">
+                        Rp. {{ number_format($order->biaya_ongkir, 0, ',', '.') }} <br>
+                        {{ $order->kurir.'.'.$order->layanan_ongkir.' *estimasi '. $order->estimasi_ongkir.' Hari'}}
+                        @if(session('origin'))
+                        <p>Kota asal: {{ $originName }} </p>
+                        @endif
+                    </td>
+                </tr>
+                <tr>
+                    <th class="empty" colspan="3"></th>
+                    <th>TOTAL BAYAR</th>
+                    <th colspan="2" class="total">Rp. {{ number_format($totalHarga + $order->biaya_ongkir, 0, ',', '.') }}</th>
+                </tr>
+            </tfoot>
         </table>
-        <form action="{{ route('order.select-shipping') }}" method="post">
-            @csrf
-            <input type="hidden" name="total_price" value="{{ $totalHarga }}">
-            <input type="hidden" name="total_weight" value="{{ $totalBerat }}">
-            <div class="pull-right">
-                <button class="primary-btn">Pilih Pengiriman</button>
-            </div>
-        </form>
+        <input type="hidden" name="total_price" value="{{ $totalHarga }}">
+        <input type="hidden" name="total_weight" value="{{ $totalBerat }}">
+        <div class="pull-right">
+            <button class="primary-btn" id="pay-button">Bayar Sekarang</button>
+        </div>
         @else
         <p>Keranjang belanja kosong.</p>
         @endif
     </div>
 </div>
-<!-- end template-->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -83,14 +83,26 @@ Route::middleware('is.customer')->group(function () {
     // Route untuk menampilkan halaman akun customer
     Route::get('/customer/akun/{id}', [CustomerController::class, 'akun'])
         ->name('customer.akun');
+
     // Route untuk mengupdate data akun customer
     Route::put('/customer/updateakun/{id}', [CustomerController::class, 'updateAkun'])
         ->name('customer.updateakun');
+
     // Route untuk menambahkan produk ke keranjang
     Route::post('add-to-cart/{id}', [OrderController::class, 'addToCart'])
         ->name('order.addToCart');
     Route::get('cart', [OrderController::class, 'viewCart'])
         ->name('order.cart');
+    Route::post('cart/update/{id}', [OrderController::class, 'updateCart'])
+        ->name('order.updateCart');
+    Route::post('remove/{id}', [OrderController::class, 'removeFromCart'])
+        ->name('order.remove');
+    Route::post('select-shipping', [OrderController::class, 'selectShipping'])
+        ->name('order.select-shipping');
+    Route::post('update-ongkir', [OrderController::class, 'updateOngkir'])
+        ->name('order.update-ongkir');
+    Route::get('select-payment', [OrderController::class, 'selectPayment'])
+        ->name('order.selectpayment');
 });
 
 // Route untuk API Raja Ongkir
@@ -99,17 +111,15 @@ Route::get('/rajaongkir_list1', function () {
         'key' => '794a5d197b9cb469ae958ed043ccf921'
     ])->get('https://api.rajaongkir.com/starter/province'); //bisa ganti dengan 'province' atau 'city'
     // $statusCode = $response->json()['rajaongkir']['status']['code'];
-    // $provisi = $response->json()['rajaongkir']['results'];
+    // $provinsi = $response->json()['rajaongkir']['results'];
     dd($response->json());
 });
-
 Route::get('/rajaongkir_list2', function () {
     $response = Http::withHeaders([
         'key' => '794a5d197b9cb469ae958ed043ccf921'
-    ])->get('https://api.rajaongkir.com/starter/province'); //bisa ganti dengan 'province' atau 'city'
+    ])->get('https://api.rajaongkir.com/starter/city'); //bisa ganti dengan 'province' atau 'city'
     return $response->json();
 });
-
 
 Route::get('/cek-ongkir', function () {
     return view('ongkir');
